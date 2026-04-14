39f9cdc5e3cfd644a5af4a00f504a3115dff9b73
idk what to name this profile <visoshell@gmail.com>
2026-04-14 17:53:54 -0400

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,20 +1,17 @@
 @echo off
 setlocal
 
-:: 1. Move to the directory where the script is located
 cd /d "%~dp0"
 
-:: 2. Define paths relative to the project root
 set "SOURCE_FILE=main.c"
 set "OUTPUT_DIR=obj\Release"
 set "OUTPUT_FILE=%OUTPUT_DIR%\main.o"
 
-:: 3. Create the directory if it doesn't exist
 if not exist "%OUTPUT_DIR%" (
     mkdir "%OUTPUT_DIR%"
 )
 
-:: 4. Run the compiler
+
 echo Building %OUTPUT_FILE%...
 gcc -c "%SOURCE_FILE%" -o "%OUTPUT_FILE%" -O2
 
